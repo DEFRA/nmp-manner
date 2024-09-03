9ea4e34d1925dbab1ef865e593f42917c4d47ab7
Shaun Brown <shaun.brown@rsk-bsl.com>
2024-09-03 10:41:22 +0100

    Updated SubSoil and TopSoil to include VolumetricMeasure and AvailableWaterCapacity, as used in original manner.

--- Manner.Api/Manner.Core/Entities/TopSoil.cs
+++ Manner.Api/Manner.Core/Entities/TopSoil.cs
@@ -3,4 +3,6 @@ public class TopSoil
 {
     public int ID { get; set; }
     public string Name { get; set; }
+    public int VolumetricMeasure { get; set; } // New property
+    public int AvailableWaterCapacity { get; set; } // New property
 }
